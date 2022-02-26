8f6d6ca5e24d27e74fcdf55f4fd14e2e1486fea1
Eliab Lopez <eliablopez@hotmail.com>
2022-02-26 18:35:26 -0500

    Express initial configuratión

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import connectDB from './database/db';
+import routerApi from './routes';
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+routerApi(app);
+
+app.listen(PORT, () => {
+  console.log(`El servidor esta corriendo en el puerto ${PORT}`);
+});
